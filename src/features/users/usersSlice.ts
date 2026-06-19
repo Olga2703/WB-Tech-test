9b5fb27780bf039c4eee8f35e4ba24e6d4e24c83
opetrenko <opetrenko@it-solutions18.ru>
2026-06-19 12:46:14 +0400

    feat: add all thunks into slice

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,6 +1,6 @@
 import type {User} from "../../types/user.ts";
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {fetchUsers} from "./usersThunks.ts";
+import {createUser, deleteUser, fetchUserById, fetchUsers, updateUser} from "./usersThunks.ts";
 
 interface UsersState {
     items: User[];
@@ -8,6 +8,7 @@ interface UsersState {
     error: string | null;
     totalCount: number;
     currentPage: number;
+    currentUser: User | null;
 }
 
 const initialState: UsersState = {
@@ -16,6 +17,7 @@ const initialState: UsersState = {
     error: null,
     totalCount: 0,
     currentPage: 1,
+    currentUser: null,
 };
 
 const usersSlice = createSlice({
@@ -41,5 +43,57 @@ const usersSlice = createSlice({
                 state.isLoading = false;
                 state.error = action.payload || 'Произошла ошибка';
             })
+            .addCase(fetchUserById.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+                state.currentUser = null;
+            })
+            .addCase(fetchUserById.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.currentUser = action.payload;
+            })
+            .addCase(fetchUserById.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload || 'Не удалось загрузить данные пользователя';
+            })
+            .addCase(createUser.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+            })
+            .addCase(createUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.items.unshift(action.payload);
+                state.totalCount += 1;
+            })
+            .addCase(createUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload || 'Не удалось создать пользователя';
+            })
+            .addCase(updateUser.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+            })
+            .addCase(updateUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                const index = state.items.findIndex((item) => item.id === action.payload.id);
+                if (index) {
+                    state.items[index] = action.payload;
+                }
+            })
+            .addCase(updateUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload || 'Не удалось обновить данные пользователя'ж
+            })
+            .addCase(deleteUser.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+            })
+            .addCase(deleteUser.fulfilled, (state, action) => {{
+                state.items = state.items.filter((item) => item.id !== action.payload);
+                state.totalCount -= 1;
+            }});
     }
-})
+});
+
+export const { setCurrentPage } = usersSlice.actions;
+export default usersSlice.reducer;
